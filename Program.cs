6efd4789a468203a00caf3c915c2397826774667
Md. Sukel Ali <sukelali@gmail.com>
2024-07-30 11:43:27 +0600

    :star: Add Reverse String Class

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using interview_practice.topics;
+
+
+
+while(true)
+{
+
+
+    Console.WriteLine("Enter Input: ");
+
+    var str =  Console.ReadLine();
+
+    var reverseString = StringReverse.reverse(str); 
+
+    Console.WriteLine($"Reverse String : { reverseString }");
+
+
+}
---
+++ topics/StringReverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interview_practice.topics
+{
+    public static class StringReverse
+    {
+
+
+        public static string reverse(string str) {
+
+            char[] strChar = str.ToCharArray();
+
+            StringBuilder newString = new StringBuilder();
+
+            for(int i = 0, j = strChar.Length - 1 ; i < strChar.Length; i++, j--)
+            {
+
+                newString.Append(strChar[j]);
+            }
+
+            return newString.ToString();    
+        }
+
+    }
+}
